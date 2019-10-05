9c226a932936083b4042033cb8590c81205de084
takumifunayama <mt.koala01n@gmail.com>
2019-10-05 06:30:19 +0000

    9.5削除機能の追加まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'microposts/create'
+  get 'microposts/destroy'
   root to:'toppages#index'
   
   get 'login', to:'sessions#new'
   post 'login', to:'sessions#create'
   delete'logout',to:'sessions#destroy'
-  
+
   get 'signup',to:'users#new'
   resource :users, only: [:index,:show,:new, :create]
+  
+  resource :microposts, only:[:create,:destroy]
 end
